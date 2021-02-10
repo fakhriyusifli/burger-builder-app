9634f586ad20be6c34141a0f2875b195eeb2d27b
Fakhri Yusifli <fakhriyusifli@gmail.com>
2021-02-10 18:24:28 +0400

    build controls buttons

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,8 +11,17 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong>{props.price.toFixed(2)} $ </strong>
+    </p>
     {controls.map((ctrl) => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        added={() => props.ingredientAdded(ctrl.type)}
+        key={ctrl.label}
+        label={ctrl.label}
+        disabled={props.disabled[ctrl.type]}
+      />
     ))}
   </div>
 );
