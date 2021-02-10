3592453d4a9ff9ddf548e64ac025056f0e05db5d
Fakhri Yusifli <fakhriyusifli@gmail.com>
2021-02-10 23:12:24 +0400

    modal & backdrop components added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -72,6 +73,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Fragment>
+        <Modal />
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
